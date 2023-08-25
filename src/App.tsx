a22f57065362e3d5b3ef3cefeb054708594ac9a8
Mamoud Elawadi <mahmoudwagdi86@gmail.com>
2023-08-25 09:40:13 +0200

    chore: add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import TextExample from 'components/TextExample/TextExample';
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextExample />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
---
+++ src/components/TextExample/TextExample.tsx
@@ -0,0 +1,6 @@
+import { Text } from 'react-native';
+
+const TextExample = () => (
+  <Text>Open up App.tsx to start working on your app!kll</Text>
+);
+export default TextExample;
